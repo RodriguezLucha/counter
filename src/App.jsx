76a509f039df4e0d9ff0517a139804acde7aa9e4
Rudy <RodriguezLucha@gmail.com>
2019-05-30 22:21:21 -0700

    add different styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
 import React, {Component} from 'react';
 import './App.css';
-import { AwesomeButton } from "react-awesome-button";
-import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+import './Button.css';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      operation: 'Increment',
       counter: 0
     };
   }
   increment(){
     this.setState({counter: this.state.counter + 1});
   }
-  decrement(){
+  decrement(e){
+    e.stopPropagation();
     this.setState({counter: this.state.counter - 1});
   }
 
-  toggleOperation(e){
-    e.stopPropagation();
-    this.state.operation === 'Increment' ? this.setState({operation:'Decrement'}) : this.setState({operation:'Increment'})
-  }
-  
   resetCounter(e){
     e.stopPropagation();
     this.setState({counter: 0});
@@ -31,17 +25,17 @@ export default class App extends Component {
 
   render(){
     return (
-      <div className="App" onClick={() => this.state.operation === 'Increment' ? this.increment() : this.decrement()}>
+      <div className="App" onClick={() => this.increment()}>
         <div className="counter" >
           {this.state.counter}
         </div>
         <div className='button-house'>
-          <AwesomeButton cssModule={AwesomeButtonStyles} onClick={(e) => this.resetCounter(e)} >
+          <button className="button" onClick={(e) => this.resetCounter(e)}>
             Reset
-          </AwesomeButton>
-          <AwesomeButton cssModule={AwesomeButtonStyles} onClick={(e) => this.toggleOperation(e)} >
-            {this.state.operation}
-          </AwesomeButton>
+          </button>
+          <button className="button" onClick={(e) => this.decrement(e)}>
+            -
+          </button>
         </div>
       </div>
     );
